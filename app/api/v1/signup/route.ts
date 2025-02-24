85d279240d9996d2bd2bcab45a97158999b09611
Rahul NS <rahul9380767545@gmail.com>
2025-02-24 20:49:16 +0530

    Update signup page

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     const data = await req.json();
 
-    
     console.log(data);
 
     return NextResponse.json({
         message: "You have been signed up"
-    })
+    });
+    
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Signin() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter();
 
     return <div className="w-screen h-screen flex justify-center items-center">
         <div className="border p-2">
@@ -20,6 +22,9 @@ export default function Signin() {
                     username,
                     password
                 });
+
+                router.push("/signin");
+
             }}>Sign up</button>
         </div>
     </div>
